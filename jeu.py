fab6848425ae9576da401e90ed8f084c6de88eae
vava0406 <evaguillou04@gmail.com>
2024-01-30 14:21:25 +0100

    Update jeu.py

--- jeu.py
+++ jeu.py
@@ -1,6 +1,6 @@
 """
 Snake Eater
-Made with PyGame1
+Made with PyGame
 """
 
 import pygame, sys, time, random
@@ -15,8 +15,8 @@ import pygame, sys, time, random
 difficulty = 25
 
 # Window size
-frame_size_x = 720
-frame_size_y = 480
+FRAME_SIZE_Y = 720
+FRAME_SIZE_X = 480
 
 # Checks for errors encountered
 check_errors = pygame.init()
